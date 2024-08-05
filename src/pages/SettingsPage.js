ac1acc383aeab56253842edfb08bd840fc1c5901
FENA2004 <149611078+FENA2004@users.noreply.github.com>
2024-08-05 22:58:35 +0530

    Altered SettingsPage UI

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,7 +1,7 @@
-// src/pages/Settings.js
-import React from 'react';
-import useStore from '../store/UseStore';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useStore from '../store/UseStore';
+import '../SettingsPage.css'
 
 const Settings = () => {
   const { background, boxColor, fontColor, setColors } = useStore();
@@ -23,20 +23,22 @@ const Settings = () => {
 
   return (
     <div className="settings-page">
-      <h1>Settings</h1>
-      <label>
-        Background Color:
-        <input type="color" value={background} onChange={handleChangeBackground} />
-      </label>
-      <label>
-        Box Color:
-        <input type="color" value={boxColor} onChange={handleChangeBoxColor} />
-      </label>
-      <label>
-        Font Color:
-        <input type="color" value={fontColor} onChange={handleChangeFontColor} />
-      </label>
-      <button onClick={handlePlayGame}>Play Game</button>
+      <div className="settings-container">
+        <h1>Settings</h1>
+        <label>
+          Background Color:
+          <input type="color" value={background} onChange={handleChangeBackground} />
+        </label>
+        <label>
+          Box Color:
+          <input type="color" value={boxColor} onChange={handleChangeBoxColor} />
+        </label>
+        <label>
+          Font Color:
+          <input type="color" value={fontColor} onChange={handleChangeFontColor} />
+        </label>
+        <button onClick={handlePlayGame}>Play Game</button>
+      </div>
     </div>
   );
 };
